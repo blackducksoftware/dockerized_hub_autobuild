2a88f70289b7b9e984ea9bb9d2357b04f5752a60
Bob Lloyd <blloyd@blackducksoftware.com>
2016-10-25 09:43:36 -0400

    Add debugging info to find out where we're failing to install.

--- hub/installNoLicense.sh
+++ hub/installNoLicense.sh
@@ -4,19 +4,25 @@
 
 echo "HUB install starting"
 
+echo "Making the /opt/blackduck/install folder"
 mkdir -p /opt/blackduck/install
 
+echo "Copying the installation files into the /opt/blackduck/install folder"
 cp -r /tmp/hub-install /opt/blackduck/install
 
+echo "Starting the installation procedures"
 # start installation
 find /opt/blackduck/install -name "appmgr.hubinstall" -execdir {} -sf /opt/blackduck/install/silentInstall.properties \; 
 
+echo "Stop the Hub"
 # stop hub
 /opt/blackduck/hub/appmgr/bin/hubcontrol.sh stop
 
+echo "Start the Hub"
 # start hub
 /opt/blackduck/hub/appmgr/bin/hubcontrol.sh start
 
+echo "Cleanup the licensing"
 # remove all license related stuff
 rm -rf /opt/blackduck/hub/logs/appmgr/bd-AppmgrAgent.log*
 rm -rf /opt/blackduck/hub/bd-install_log.txt
@@ -24,5 +30,6 @@ rm -rf /opt/blackduck/hub/bd-install_log.txt
 # remove license file
 rm -rf /opt/blackduck/hub/config/suite_v1.xml
 
+echo "Remove the installation folder when we're done"
 # remove installation directory
 rm -rf /opt/blackduck/install
