3cf953c53cfe5a5ee615b0240aa686f5953274df
Bob Lloyd <blloyd@blackducksoftware.com>
2016-10-25 11:55:05 -0400

    Fix missing underscore in _ON_PREM

--- hub/ci-autobuildNoLicense.sh
+++ hub/ci-autobuildNoLicense.sh
@@ -108,7 +108,7 @@ find . -name "silentInstall.properties" -exec sed -i "$ a\PROP_ACTIVE_REGID=${_L
 scp -r . serv-builder@eng-ddc-node01.dc1.lan:~/hub-install/.
 
 #start initial image with the install script
-if [ "$_ONPREM" != "true" ]; then
+if [ "$_ON_PREM" != "true" ]; then
   echo "Not an on-prem build"
   docker run -i --sysctl kernel.shmmax=323485952 --label node:${_DOCKER_NODE} --name=$_CONTAINER_NAME -v /home/serv-builder/hub-install:/tmp/hub-install -p 4181:4181 -p 8080:8080 -p 7081:7081 -p 55436:55436 -p 8009:8009 -p 8993:8993 -p 8909:8909 $_TMP_IMG_NAME /tmp/hub-install/installNoLicense.sh
 else
