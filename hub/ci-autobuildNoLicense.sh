6164e78af736842872d302d888dacd6a8a2fd68d
Bob Lloyd <blloyd@blackducksoftware.com>
2016-10-25 09:33:42 -0400

    Set the volume to the correct folder, and set the execution to run from that folder to start.

--- hub/ci-autobuildNoLicense.sh
+++ hub/ci-autobuildNoLicense.sh
@@ -89,7 +89,7 @@ docker build  --no-cache \
 if [ "$?" != "0" ]; then exit $?; fi
 
 #start initial image with the install script
-docker run -i --sysctl kernel.shmmax=323485952 --label node:${_DOCKER_NODE} --name=$_CONTAINER_NAME -v /tmp/hub-install:/opt/blackduck/install -p 4181:4181 -p 8080:8080 -p 7081:7081 -p 55436:55436 -p 8009:8009 -p 8993:8993 -p 8909:8909 $_TMP_IMG_NAME /opt/blackduck/install/installNoLicense.sh
+docker run -i --sysctl kernel.shmmax=323485952 --label node:${_DOCKER_NODE} --name=$_CONTAINER_NAME -v /home/serv-builder/hub-install:/tmp/hub-install -p 4181:4181 -p 8080:8080 -p 7081:7081 -p 55436:55436 -p 8009:8009 -p 8993:8993 -p 8909:8909 $_TMP_IMG_NAME /tmp/hub-install/installNoLicense.sh
 
 if [ "$?" != "0" ]; then exit $?; fi
 
